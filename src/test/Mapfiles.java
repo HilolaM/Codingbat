f632d2526784bff74f492a9aea477a91ba2f5de5
HilolaM <hnew.moon@gmail.com>
2017-11-23 13:37:30 -0800

    56

--- src/test/Mapfiles.java
+++ src/test/Mapfiles.java
@@ -7,7 +7,7 @@ public class Mapfiles {
 		System.out.println("hello world");
 		System.out.println("hello earth!");
 		
-//making changes here added few items to print
+//making changes 
 		//
 	}
 
